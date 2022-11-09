aa6bca93f254ed99051522d9f9935734e6c1fa6e
Yowgsiavka <107213498+Yowgsiavka@users.noreply.github.com>
2022-11-09 15:35:31 +0330

    Update url.py

--- agz/url.py
+++ agz/url.py
@@ -3,9 +3,9 @@ from socket import socket
 from requests import get
 class Url:
 	
-	_socket = "wss://shsocket9.iranlms.ir:80"
-	_api = "https://shadmessenger139.iranlms.ir/"
-	_storange = "https://shstorage527.iranlms.ir/"
+	_socket = "wss://shsocket1.iranlms.ir:80"
+	_api = "https://shadmessenger48.iranlms.ir"
+	_storange = "https://shstorage501.iranlms.ir"
 	
 	def data():
 		urls = get('https://shgetdcmess.iranlms.ir/').json()
